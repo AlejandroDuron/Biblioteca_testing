fa3520fff656769104b1376d23824d6ab89c8920
Reina <reinasosa2024@gmail.com>
2026-03-06 18:06:51 -0600

    tests para auth

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -1,36 +1,117 @@
 <?php
 
-namespace Tests\Feature;
-
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
 
-class AuthTest extends TestCase
-{
-    use RefreshDatabase;
+uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 
-    public function test_it_can_login()
-    {
-        // Preparacion
-        $user = User::factory()->create([
-            'password' => bcrypt('test123'),
-        ]);
+//Pruebas de distintos escenarios de incio de sesion
+// Prueba 1: 
+it('can log in', function () {
+    // Preparacion
+    $user = User::factory()->create([
+        'password' => bcrypt('test123'),
+    ]);
 
-        // Ejecucion
-        $response = $this->post('/api/v1/login', [
+    // Ejecucion
+    $response = $this->post('/api/v1/login', [
+        'email' => $user->email,
+        'password' => 'test123',
+    ]);
+
+    // Verificacion
+    $response->assertStatus(200);
+    $response->assertJsonStructure([
+        'access_token',
+        'token_type',
+        'user',
+    ]);
+
+    $this->assertAuthenticatedAs($user);
+});
+
+//Prueba 2:
+it('it cannot log in with invalid password', function () {
+    // Preparacion
+    $user = User::factory()->create([
+        'password' => bcrypt('test123'),
+    ]);
+
+    // Ejecucion
+    $response = $this->post('/api/v1/login', [
+        'email' => $user->email,
+        'password' => 'password567',
+    ]);
+
+    // Verificacion
+    $response->assertStatus(422);
+
+    $response->assertJson([
+        'message' => 'Invalid credentials'
+    ]);
+
+    $this->assertGuest();
+});
+
+//Prueba 3:
+it('it cannot log in with invalid email', function () {
+    //Ejecucion
+    $response = $this->post('/api/v1/login', [
+        'email' => 'emailincorrecto@correo.com',
+        'password' => 'escuela123',
+    ]);
+
+    // Verificacion
+    $response->assertStatus(422);
+
+    $response->assertJson([
+        'message' => 'Invalid credentials'
+    ]);
+
+    $this->assertGuest();
+});
+
+//Pruebas de distintos escenarios de cierre de sesion
+//Prueba 4:
+it('can log out', function () {
+    $user = User::factory()->create();
+    $response = $this->actingAs($user)->post('/api/v1/logout');
+
+    $response->assertStatus(200);
+    $response->assertJson([
+        'message' => 'Logged out successfully'
+    ]);
+
+    expect($user->tokens()->count())->toBe(0);
+});
+
+//Prueba 5:
+it('unauthenticated users cannot log out', function () {
+    $response = $this->postJson('/api/v1/logout');
+
+    $response->assertStatus(401);
+});
+
+//Pruebas de distintos escenarios de mostrar el perfil del usuario
+//Prueba 6:
+it('it shows user profile', function () {
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->get('/api/v1/profile');
+
+    //Verificacion
+    $response->assertStatus(200);
+    $response->assertJson([
+        'user' => [
+            'id' => $user->id,
+            'name' => $user->name,
             'email' => $user->email,
-            'password' => 'test123',
-        ]);
+        ]
+    ]);
+});
 
-        // Verificacion
-        $response->assertStatus(200);
-        $response->assertJsonStructure([
-            'access_token',
-            'token_type',
-            'user',
-        ]);
+//Prueba 7:
+it('should not show users without active session', function () {
+    $response = $this->getJson('/api/v1/profile');
 
-        $this->assertAuthenticatedAs($user);
-    }
-}
+    $response->assertStatus(401);
+});
