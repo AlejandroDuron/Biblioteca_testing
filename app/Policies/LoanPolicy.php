9451f40bcc95f3ecfb97c77c1b24c47935059217
Reina <reinasosa2024@gmail.com>
2026-03-06 20:24:54 -0600

    corriendo  policy

--- app/Policies/LoanPolicy.php
+++ app/Policies/LoanPolicy.php
@@ -2,14 +2,25 @@
 
 namespace App\Policies;
 
+use App\Models\Loan;
 use App\Models\User;
 
 class LoanPolicy
 {
+    public function viewAny(User $user): bool
+    {
+        return $user->hasAnyRole(['Estudiante', 'Docente', 'Bibliotecario']);
+    }
+
     /**
      * Determine whether the user can create models.
      */
     public function create(User $user): bool
+    {
+        return $user->hasAnyRole(['Estudiante', 'Docente']);
+    }
+
+    public function update(User $user, Loan $loan): bool
     {
         return $user->hasAnyRole(['Estudiante', 'Docente']);
     }
